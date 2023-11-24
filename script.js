5c05dcd98a4e23c9b6416842c97ddc76bd58f056
Jakob Skjerning <jakob@mentalized.net>
2023-11-24 12:55:09 +0100

    Set up Phaser

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// Called by Phaser on initialization
+const create = () => {
+  this.scale.on(
+    'resize',
+    function (gameSize) {
+      const width = gameSize.width;
+      const height = gameSize.height;
+
+      this.cameras.resize(width, height);
+    },
+    this
+  );
+}
+
 const getWindowsFromLocalStorage = () => {
   const windows = JSON.parse(localStorage.getItem('windows'));
   return windows;
@@ -18,6 +32,10 @@ const setWindowsFromLocalStorage = (windows) => {
   localStorage.setItem("windows", JSON.stringify(windows));
 }
 
+// Called by Phaser on every frame
+const update = () => {
+}
+
 const windowIdentifier = () => {
   return self.window.name || Math.random().toString(36).slice(2, 12);
 }
@@ -34,7 +52,7 @@ const removeWindow = () => {
   removeFromWindows(identifier);
 }
 
-const update = () => {
+const logDimensions = () => {
   const { width, height } = visualViewport;
   const { screenX: left, screenY: top } = window;
 
@@ -60,6 +78,26 @@ const update = () => {
   });
 }
 
-setInterval(update, 1000);
+const config = {
+  mode: Phaser.Scale.NONE,
+  type: Phaser.AUTO,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  scene: {
+    create: create,
+    update: update,
+  }
+}
+const game = new Phaser.Game(config);
+
+setInterval(logDimensions, 1000);
 
 window.onbeforeunload = removeWindow;
+
+window.addEventListener('resize', event => {
+  // In scaleMode NONE the Scale Manager is effectively disabled, so we need
+  // to tell Phaser when a resize happens.
+  //
+  // This triggers the scale.resize event, which we listen to in Phaser.
+  game.scale.resize(window.innerWidth, window.innerHeight);
+}, false);
